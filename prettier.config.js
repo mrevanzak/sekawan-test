f85b4f783882845a4c30aca62e14d22577a3ef5a
mrevanzak <mrevanzak@gmail.com>
2024-05-07 17:31:46 +0700

    chore: change prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,6 @@ module.exports = {
   printWidth: 100,
   tabWidth: 2,
   singleQuote: true,
-  bracketSameLine: true,
   trailingComma: 'es5',
 
   plugins: [require.resolve('prettier-plugin-tailwindcss')],
