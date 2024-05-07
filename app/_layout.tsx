64e26cfac610d8d1f49655545505dc14949db99e
mrevanzak <mrevanzak@gmail.com>
2024-05-07 13:01:18 +0700

    feat: clerk setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,21 @@
 import '../global.css';
 
+import { ClerkProvider } from '@clerk/clerk-expo';
+import Constants from 'expo-constants';
 import { Stack } from 'expo-router';
 
+import { tokenCache } from '@/utils/cache';
+
 export default function Layout() {
-  return <Stack />;
+  if (!Constants.expoConfig?.extra?.clerkPublishableKey) {
+    throw new Error('Missing Clerk Publishable Key');
+  }
+
+  return (
+    <ClerkProvider
+      publishableKey={Constants.expoConfig.extra.clerkPublishableKey}
+      tokenCache={tokenCache}>
+      <Stack />
+    </ClerkProvider>
+  );
 }
